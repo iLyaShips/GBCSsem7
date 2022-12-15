da6d24c455d5cb95804b3831ae092de6dd3626be
iLya <andr@google.com>
2022-12-15 16:51:12 +0300

    commit add 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j].ToString("F" + 1),3}\t");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = (new Random().NextDouble() * 2 - 1)*100;
+}
+
+try
+{
+    Console.WriteLine("Введите количество строк двумерного массива");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов двумерного массива");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    double[,] array = new double[rows, cols];
+    FillArray(array);
+    Console.WriteLine("");
+    PrintArray(array);
+}
+catch
+{
+    Console.WriteLine("Ошибка! Проверьте вводимые данные.");
+}
